ed26751cfe75acb5563601a28e7e922141755142
Julia Doroshina <up.doroshina@yandex.ru>
2021-02-05 17:17:46 +0300

    feat: add MainApi

---
+++ src/configs/api.js
@@ -0,0 +1,10 @@
+// API для регистрации/авторизауии, сохранения данных пользователя
+const MAIN_API_BASE_URL = 'https://api.wow-news.students.nomoredomains.icu/';
+
+// API для получения новостей
+const NEWS_API_BASE_URL = '';
+
+export {
+  NEWS_API_BASE_URL,
+  MAIN_API_BASE_URL,
+};
