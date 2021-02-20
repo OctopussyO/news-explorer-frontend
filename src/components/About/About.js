7c9936afdd4e3d95e950e14039025a7d22cc9209
Julia Doroshina <up.doroshina@yandex.ru>
2021-02-20 15:38:41 +0300

    feat: add about myself text

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,6 +9,26 @@ const About = () => {
   const { pageNarrowClassName } = useContext(CommonPageStylesContext);
   const sectionClassName = joinCN({ basic: ['about', pageNarrowClassName] });
 
+  const renderInfo = () => {
+    return authorConfig.info.map((el, i) => {
+      if (el.type === 'text') {
+        return (
+          <p className='about__paragraph about__text typo typo_font-family_roboto' key={i}>
+            { el.text }
+          </p>
+        );
+      } else if (el.type === 'list') {
+        return (
+          <ul className='about__list about__text typo typo_font-family_roboto' key={i}>
+            { el.text.split(';').map((el, i, arr) => (
+              <li key={el}>{el}{i !== arr.length - 1 ? ',' : '.'}</li>
+            )) }
+          </ul>
+        );
+      } else return null;
+    });
+  };
+
   return (
     <section className={sectionClassName}>
       <img className="about__avatar" src={authorConfig.avatar} alt="Фотография автора" />
@@ -16,11 +36,7 @@ const About = () => {
         <h3 className="about__title typo typo_font-family_roboto-slab">
           Об авторе
         </h3>
-        { authorConfig.info.map((el, i) => (
-          <p className="about__paragraph typo typo_font-family_roboto" key={i}>
-            { el }
-          </p>
-        )) }
+        { renderInfo() }
       </div>
     </section>
   );
